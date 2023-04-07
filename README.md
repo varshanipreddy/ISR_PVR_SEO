60a34ca5d562782ae843d0245e9c471031c5fff4
varshani reddy <varshanireddy@varshanis-MacBook-Pro.local>
2023-04-07 17:06:45 -0500

    update readme and blog

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# ISR_PVR_SEO
+# wpaskjbdn zqnakjsnde
+Wpaskjbdn zqnakjsnde is a term that has gained popularity among students taking the ISR course at Texas A&M University. The term is used as part of an SEO contest, which is a competition where students try to rank their webpages higher in search engine results pages (SERPs) than their competitors for the specific keyword - wpaskjbdn zqnakjsnde.
+
+The wpaskjbdn zqnakjsnde contest requires students to use various SEO strategies to improve the ranking of their website in search engine results. Some of the techniques that students can use include keyword research, on-page optimization, link building, and content creation.
+
+One of the keys to succeeding in the wpaskjbdn zqnakjsnde competition is to have a well-structured website with high-quality content that includes the keyword wpaskjbdn zqnakjsnde. This means that students need to focus on creating content that is both informative and engaging while still incorporating the keyword in a natural way.
+
+Students participating in the wpaskjbdn zqnakjsnde competition should also pay attention to the technical aspects of their website. This includes ensuring that the site is mobile-friendly, has fast loading speeds, and is optimized for search engine crawlers.
+
+Another crucial aspect of the wpaskjbdn zqnakjsnde competition is link building. Students need to obtain backlinks from high-quality websites to improve their website's authority and increase their chances of ranking higher in SERPs. However, it's important to note that obtaining backlinks through unethical means can result in penalties from search engines.
+
+To improve their chances of winning the wpaskjbdn zqnakjsnde competition, students can also participate in social media marketing and other forms of online promotion. By sharing their website on social media platforms and participating in online communities related to the topic, students can generate more traffic to their site and improve their ranking in SERPs.
+
+The wpaskjbdn zqnakjsnde competition provides students with a valuable opportunity to gain hands-on experience with SEO and digital marketing. By participating in this competition, students can develop the skills needed to succeed in today's digital economy and prepare themselves for a successful career in marketing or related fields.
+
+Overall, the wpaskjbdn zqnakjsnde competition is an exciting challenge that requires students to apply their knowledge and creativity to improve their website's ranking in search engine results pages. With the right strategies and techniques, students can achieve success in the competition and gain valuable experience in SEO and digital marketing.
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,19 +1,48 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import './HomePage.css';
+
 
 function HomePage() {
   return (
     <div className="container">
-      <h1>Howdy!! Welcome to my wpaskjbdn zqnakjsnde.. </h1>
-      <p>This is my page about wpaskjbdn zqnakjsnde!!!!!</p>
-      <p1> I am Varshani Reddy !!</p1>
-      <div className="button-container">
-        <input type="text" placeholder="Search" className="search-bar" />
-        <button className="button">doesn't</button>
-        <button className="button button-right">work</button>
-      </div>
+      <Helmet>
+        <title>WPASKJBDN ZQNAKJSNDE - SEO Contest | Texas A&M University</title>
+        <meta name="description" content="Learn about the wpaskjbdn zqnakjsnde SEO contest at Texas A&M University and discover the strategies and techniques used by students to rank higher in search engine results pages." />
+        <meta name="keywords" content="wpaskjbdn zqnakjsnde, seo contest, texas a&m university, keyword research, link building, content creation, digital marketing, search engine results pages" />
+      </Helmet>
+    <div className="heading">
+      <h1>WPASKJBDN ZQNAKJSNDE</h1>
     </div>
+    <div className="content">
+      <p>
+        Wpaskjbdn zqnakjsnde is a term that has gained popularity among students taking the ISR course at Texas A&M University. The term is used as part of an SEO contest, which is a competition where students try to rank their webpages higher in search engine results pages (SERPs) than their competitors for the specific keyword - wpaskjbdn zqnakjsnde.      
+      </p>
+      <p>
+        The wpaskjbdn zqnakjsnde contest requires students to use various SEO strategies to improve the ranking of their website in search engine results. Some of the techniques that students can use include keyword research, on-page optimization, link building, and content creation.      
+      </p>
+      <p>
+        One of the keys to succeeding in the wpaskjbdn zqnakjsnde competition is to have a well-structured website with high-quality content that includes the keyword wpaskjbdn zqnakjsnde. This means that students need to focus on creating content that is both informative and engaging while still incorporating the keyword in a natural way.
+      </p>
+      <p>
+        Students participating in the wpaskjbdn zqnakjsnde competition should also pay attention to the technical aspects of their website. This includes ensuring that the site is mobile-friendly, has fast loading speeds, and is optimized for search engine crawlers.
+      </p>
+      <p>
+        Another crucial aspect of the wpaskjbdn zqnakjsnde competition is link building. Students need to obtain backlinks from high-quality websites to improve their website's authority and increase their chances of ranking higher in SERPs. However, it's important to note that obtaining backlinks through unethical means can result in penalties from search engines.      
+      </p>
+      <p>
+        To improve their chances of winning the wpaskjbdn zqnakjsnde competition, students can also participate in social media marketing and other forms of online promotion. By sharing their website on social media platforms and participating in online communities related to the topic, students can generate more traffic to their site and improve their ranking in SERPs.      
+      </p>
+      <p>
+        The wpaskjbdn zqnakjsnde competition provides students with a valuable opportunity to gain hands-on experience with SEO and digital marketing. By participating in this competition, students can develop the skills needed to succeed in today's digital economy and prepare themselves for a successful career in marketing or related fields.
+      </p>
+      <p>
+        Overall, the wpaskjbdn zqnakjsnde competition is an exciting challenge that requires students to apply their knowledge and creativity to improve their website's ranking in search engine results pages. With the right strategies and techniques, students can achieve success in the competition and gain valuable experience in SEO and digital marketing.
+      </p>
+    </div>
+  </div>
   );
 }
+
 
 export default HomePage;
